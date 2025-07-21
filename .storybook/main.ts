6870e46e00e5acc18971b6518530fd333be7010f
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2025-07-21 11:50:04 +0300

    errorfix: edit storybook configs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,31 @@
-import type { StorybookConfig } from '@storybook/nextjs';
 import path from 'path';
+import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-vitest',
+    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  webpackFinal: async (webpackConfig) => {
-    webpackConfig.resolve = webpackConfig.resolve || {};
-    webpackConfig.resolve.alias = {
-      ...(webpackConfig.resolve.alias || {}),
-      '@': path.resolve(__dirname, '../src'), 
-    };
-    return webpackConfig;
+  staticDirs: ['../public'],
+
+  webpackFinal: async (config) => {
+    if (config.resolve && config.resolve.alias) {
+      config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+    } else if (config.resolve) {
+      config.resolve.alias = {
+        '@': path.resolve(__dirname, '../src'),
+      };
+    }
+
+    return config;
   },
 };
 
